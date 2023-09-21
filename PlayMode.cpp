903918989a73670481608c52b1ed4591961510c8
Kevin <kevinyou@andrew.cmu.edu>
2023-09-21 14:31:44 -0400

    Game functional

--- PlayMode.cpp
+++ PlayMode.cpp
@@ -11,16 +11,19 @@
 #include <glm/gtc/type_ptr.hpp>
 
 #include <random>
+#include <queue>
+#include <sstream>
+#include <fstream>
 
 GLuint hexapod_meshes_for_lit_color_texture_program = 0;
 Load< MeshBuffer > hexapod_meshes(LoadTagDefault, []() -> MeshBuffer const * {
-	MeshBuffer const *ret = new MeshBuffer(data_path("hexapod.pnct"));
+	MeshBuffer const *ret = new MeshBuffer(data_path("real.pnct"));
 	hexapod_meshes_for_lit_color_texture_program = ret->make_vao_for_program(lit_color_texture_program->program);
 	return ret;
 });
 
 Load< Scene > hexapod_scene(LoadTagDefault, []() -> Scene const * {
-	return new Scene(data_path("hexapod.scene"), [&](Scene &scene, Scene::Transform *transform, std::string const &mesh_name){
+	return new Scene(data_path("real.scene"), [&](Scene &scene, Scene::Transform *transform, std::string const &mesh_name){
 		Mesh const &mesh = hexapod_meshes->lookup(mesh_name);
 
 		scene.drawables.emplace_back(transform);
@@ -37,31 +40,84 @@ Load< Scene > hexapod_scene(LoadTagDefault, []() -> Scene const * {
 });
 
 Load< Sound::Sample > dusty_floor_sample(LoadTagDefault, []() -> Sound::Sample const * {
-	return new Sound::Sample(data_path("dusty-floor.opus"));
+	return new Sound::Sample(data_path("beep.wav"));
 });
 
 PlayMode::PlayMode() : scene(*hexapod_scene) {
 	//get pointers to leg for convenience:
-	for (auto &transform : scene.transforms) {
-		if (transform.name == "Hip.FL") hip = &transform;
-		else if (transform.name == "UpperLeg.FL") upper_leg = &transform;
-		else if (transform.name == "LowerLeg.FL") lower_leg = &transform;
-	}
-	if (hip == nullptr) throw std::runtime_error("Hip not found.");
-	if (upper_leg == nullptr) throw std::runtime_error("Upper leg not found.");
-	if (lower_leg == nullptr) throw std::runtime_error("Lower leg not found.");
 
-	hip_base_rotation = hip->rotation;
-	upper_leg_base_rotation = upper_leg->rotation;
-	lower_leg_base_rotation = lower_leg->rotation;
+	srand(time(NULL));
+
+	// https://stackoverflow.com/questions/1894886/parsing-a-comma-delimited-stdstring
+	{
+		std::string line;
+		std::ifstream f(data_path("easy"));
+		std::getline(f, line);
+		std::stringstream ss(line);
+		for (int i; ss >> i;) {
+			easy.times.push_back(i);    
+			if (ss.peek() == ',')
+				ss.ignore();
+		}
+		std::getline(f, line);
+		ss = std::stringstream (line);
+		for (int i; ss >> i;) {
+			easy.news.push_back(i);    
+			if (ss.peek() == ',')
+				ss.ignore();
+		}
+	}
+	{
+		std::string line;
+		std::ifstream f(data_path("hard"));
+		std::getline(f, line);
+		std::stringstream ss(line);
+		for (int i; ss >> i;) {
+			hard.times.push_back(i);    
+			if (ss.peek() == ',')
+				ss.ignore();
+		}
+		std::getline(f, line);
+		ss = std::stringstream (line);
+		for (int i; ss >> i;) {
+			hard.news.push_back(i);    
+			if (ss.peek() == ',')
+				ss.ignore();
+		}
+	}
+
+	for (auto &transform : scene.transforms) {
+
+		if (transform.name.substr(0, 5) == "Frame"){
+			frame = &transform;
+			frame->position = glm::vec3(0.0f, 0.0f, 0.0f);
+		} else if (transform.name.substr(0, 5) == "Spher"){
+			sphere = &transform;
+		} else {
+			transform.position = glm::vec3(-hit_distance -10.0f, 0.0f, 0.0f);
+				if (transform.name.substr(0, 5) == "CubeA"){
+				cubea.push_back(&transform);
+			} else if (transform.name.substr(0, 5) == "CubeB"){
+				cubeb.push_back(&transform);
+			} else if (transform.name.substr(0, 5) == "CubeC"){
+				cubec.push_back(&transform);
+			} else if (transform.name.substr(0, 5) == "CubeD"){
+				cubed.push_back(&transform);
+			} else if (transform.name.substr(0, 5) == "CubeE"){
+				cubee.push_back(&transform);
+			} else if (transform.name.substr(0, 5) == "CubeF"){
+				cubef.push_back(&transform);
+			}
+		}
+	}
 
 	//get pointer to camera for convenience:
 	if (scene.cameras.size() != 1) throw std::runtime_error("Expecting scene to have exactly one camera, but it has " + std::to_string(scene.cameras.size()));
 	camera = &scene.cameras.front();
 
-	//start music loop playing:
-	// (note: position will be over-ridden in update())
-	leg_tip_loop = Sound::loop_3D(*dusty_floor_sample, 1.0f, get_leg_tip_position(), 10.0f);
+	camera->transform->rotation = default_rotation;
+	camera->transform->position = glm::vec3(-hit_distance, 0.0f, 0.0f);
+	
 }
 
 PlayMode::~PlayMode() {
@@ -73,35 +129,35 @@ bool PlayMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size)
 		if (evt.key.keysym.sym == SDLK_ESCAPE) {
 			SDL_SetRelativeMouseMode(SDL_FALSE);
 			return true;
-		} else if (evt.key.keysym.sym == SDLK_a) {
-			left.downs += 1;
-			left.pressed = true;
+		} else if (evt.key.keysym.sym == SDLK_e) {
+			button_easy.downs += 1;
+			button_easy.pressed = true;
 			return true;
-		} else if (evt.key.keysym.sym == SDLK_d) {
-			right.downs += 1;
-			right.pressed = true;
+		} else if (evt.key.keysym.sym == SDLK_h) {
+			button_hard.downs += 1;
+			button_hard.pressed = true;
 			return true;
-		} else if (evt.key.keysym.sym == SDLK_w) {
-			up.downs += 1;
-			up.pressed = true;
+		} else if (evt.key.keysym.sym == SDLK_z) {
+			button_down.downs += 1;
+			button_down.pressed = true;
 			return true;
-		} else if (evt.key.keysym.sym == SDLK_s) {
-			down.downs += 1;
-			down.pressed = true;
+		} else if (evt.key.keysym.sym == SDLK_x) {
+			button_down.downs += 1;
+			button_down.pressed = true;
 			return true;
 		}
 	} else if (evt.type == SDL_KEYUP) {
-		if (evt.key.keysym.sym == SDLK_a) {
-			left.pressed = false;
+		if (evt.key.keysym.sym == SDLK_e) {
+			button_easy.pressed = false;
 			return true;
-		} else if (evt.key.keysym.sym == SDLK_d) {
-			right.pressed = false;
+		} else if (evt.key.keysym.sym == SDLK_h) {
+			button_hard.pressed = false;
 			return true;
-		} else if (evt.key.keysym.sym == SDLK_w) {
-			up.pressed = false;
+		} else if (evt.key.keysym.sym == SDLK_z) {
+			button_down.pressed = false;
 			return true;
-		} else if (evt.key.keysym.sym == SDLK_s) {
-			down.pressed = false;
+		} else if (evt.key.keysym.sym == SDLK_x) {
+			button_down.pressed = false;
 			return true;
 		}
 	} else if (evt.type == SDL_MOUSEBUTTONDOWN) {
@@ -115,11 +171,21 @@ bool PlayMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size)
 				evt.motion.xrel / float(window_size.y),
 				-evt.motion.yrel / float(window_size.y)
 			);
-			camera->transform->rotation = glm::normalize(
-				camera->transform->rotation
-				* glm::angleAxis(-motion.x * camera->fovy, glm::vec3(0.0f, 1.0f, 0.0f))
-				* glm::angleAxis(motion.y * camera->fovy, glm::vec3(1.0f, 0.0f, 0.0f))
-			);
+			xangle = xangle - motion.x;
+			yangle = yangle + motion.y;
+			const float xb = 0.3f;
+			const float yb = 0.18f;
+			if (xangle > xb){
+				xangle = xb;
+			} else if (xangle < -xb){
+				xangle = -xb;
+			}
+			if (yangle > yb){
+				yangle = yb;
+			} else if (yangle < -yb){
+				yangle = -yb;
+			}
+
 			return true;
 		}
 	}
@@ -129,60 +195,117 @@ bool PlayMode::handle_event(SDL_Event const &evt, glm::uvec2 const &window_size)
 
 void PlayMode::update(float elapsed) {
 
-	//slowly rotates through [0,1):
-	wobble += elapsed / 10.0f;
-	wobble -= std::floor(wobble);
+	auto randf = [] () {return static_cast<float> (rand()) / (static_cast<float> (RAND_MAX));};
 
-	hip->rotation = hip_base_rotation * glm::angleAxis(
-		glm::radians(5.0f * std::sin(wobble * 2.0f * float(M_PI))),
-		glm::vec3(0.0f, 1.0f, 0.0f)
-	);
-	upper_leg->rotation = upper_leg_base_rotation * glm::angleAxis(
-		glm::radians(7.0f * std::sin(wobble * 2.0f * 2.0f * float(M_PI))),
-		glm::vec3(0.0f, 0.0f, 1.0f)
-	);
-	lower_leg->rotation = lower_leg_base_rotation * glm::angleAxis(
-		glm::radians(10.0f * std::sin(wobble * 3.0f * 2.0f * float(M_PI))),
-		glm::vec3(0.0f, 0.0f, 1.0f)
-	);
+	if (gamestate == 0){
+	
+		if (button_easy.pressed || button_hard.pressed){
+			gamestate = 1;
+			std::vector<int> times = button_easy.pressed ? easy.times : hard.times;
+			std::vector<int> news = button_easy.pressed ? easy.news : hard.news;
+			float d1 = button_easy.pressed ? 3.0f : 5.0f;
+			float d2 = button_easy.pressed ? 4.0f : 7.0f;
 
-	//move sound to follow leg tip position:
-	leg_tip_loop->set_position(get_leg_tip_position(), 1.0f / 60.0f);
+			std::queue<int> diff;
+			int count = 0;
+			for (uint32_t i=1; i<news.size(); i++){
+				if (news[i]){
+					diff.push(count);
+					count = 0;
+				} else {
+					count ++;
+				}
+			}
+			diff.push(count);
+
+			count = 0;
+			int currdiff = 0;
+			int ca = 0, cb = 0, cc = 0, cd = 0, ce = 0, cf = 0;
+			float currx = 0.0f;
+			glm::vec2 curryz = glm::vec2(0.0f), nextyz = glm::vec2(0.0f); 
+			for (uint32_t i=0; i<news.size(); i++){
+				currx += times[i] * dist_int; 
+				if (news[i]){
+					currdiff = diff.front();
+					diff.pop();
+					count = 0;
+				} else {
+					count += 1;
+				}
+				Block newblock;
+
+				if (count == 0){
+					glm::vec2 prevyz = nextyz; 
+					curryz = glm::vec2(-spawn_w + 2.0f*spawn_w * randf(), -spawn_h + 2.0f*spawn_h * randf());
+					nextyz = glm::vec2(-spawn_w + 2.0f*spawn_w * randf(), -spawn_h + 2.0f*spawn_h * randf());
+					while (glm::length(curryz - nextyz) > 3.0f * (currdiff+1) || glm::length(curryz - prevyz) > 4.0f){
+						curryz = glm::vec2(-spawn_w + 2.0f*spawn_w * randf(), -spawn_h + 2.0f*spawn_h * randf());
+						nextyz = glm::vec2(-spawn_w + 2.0f*spawn_w * randf(), -spawn_h + 2.0f*spawn_h * randf());
+					}
+					newblock.trans = cubea[ca];
+					ca++;
+				} else if (count == 1){
+					newblock.trans = cubeb[cb];
+					cb++;
+				} else if (count == 2){
+					newblock.trans = cubec[cc];
+					cc++;
+				} else if (count == 3){
+					newblock.trans = cubed[cd];
+					cd++;
+				} else if (count == 4){
+					newblock.trans = cubee[ce];
+					ce++;
+				} else if (count == 5){
+					newblock.trans = cubef[cf];
+					cf++;
+				}
+				float scalec = float(currdiff - count) / float(currdiff), scalen = float(count) / float(currdiff);
+				newblock.pos = glm::vec3(currx, curryz.x * scalec + nextyz.x * scalen, curryz.y * scalec + nextyz.y * scalen); 
+				blocks.push_back(newblock);
+			}
+			leg_tip_loop = Sound::play(*dusty_floor_sample, 1.0f, 0.0f);
+		}
+
+	} else {
+		for (uint32_t i=0; i<blocks.size(); i++){
+			if (blocks[i].active){
+				blocks[i].trans->position = blocks[i].pos;
+				blocks[i].pos.x -= velocity * elapsed; 
+
+				if (blocks[i].pos.x < -dist_int/2.0f) {
+					blocks[i].active = false;
+					blocks[i].trans->position = glm::vec3(-hit_distance -10.0f, 0.0f, 0.0f);
+					// Miss
+				} else if (blocks[i].pos.x < dist_int/2.0f){
+					if (button_down.pressed && abs(player_pos.y-blocks[i].pos.y) < 0.5f && abs(player_pos.z-blocks[i].pos.z) < 0.5f){
+						score += 100 * exp(-2*abs(player_pos.x-blocks[i].pos.x));
+						blocks[i].active = false;
+						blocks[i].trans->position = glm::vec3(-hit_distance -10.0f, 0.0f, 0.0f);
+					}
+					// Hit
+				}
+			} 
+		}
+
+	}
 
 	//move camera:
 	{
+		camera->transform->rotation = glm::angleAxis(yangle, glm::vec3(sin(xangle),-cos(xangle),0.0f))
+									* glm::angleAxis(xangle, glm::vec3(0.0f, 0.0f, 1.0f)) 
+									* default_rotation;
 
-		//combine inputs into a move:
-		constexpr float PlayerSpeed = 30.0f;
-		glm::vec2 move = glm::vec2(0.0f);
-		if (left.pressed && !right.pressed) move.x =-1.0f;
-		if (!left.pressed && right.pressed) move.x = 1.0f;
-		if (down.pressed && !up.pressed) move.y =-1.0f;
-		if (!down.pressed && up.pressed) move.y = 1.0f;
-
-		//make it so that moving diagonally doesn't go faster:
-		if (move != glm::vec2(0.0f)) move = glm::normalize(move) * PlayerSpeed * elapsed;
-
-		glm::mat4x3 frame = camera->transform->make_local_to_parent();
-		glm::vec3 frame_right = frame[0];
-		//glm::vec3 up = frame[1];
-		glm::vec3 frame_forward = -frame[2];
-
-		camera->transform->position += move.x * frame_right + move.y * frame_forward;
-	}
-
-	{ //update listener to camera position:
-		glm::mat4x3 frame = camera->transform->make_local_to_parent();
-		glm::vec3 frame_right = frame[0];
-		glm::vec3 frame_at = frame[3];
-		Sound::listener.set_position_right(frame_at, frame_right, 1.0f / 60.0f);
+		glm::vec3 dir = glm::vec3(hit_distance*cos(xangle)*cos(yangle), hit_distance*sin(xangle)*cos(yangle), hit_distance*sin(yangle));
+		player_pos = dir * (hit_distance / dir.x) - glm::vec3(hit_distance, 0.0f, 0.0f); 
+		sphere->position = player_pos;
 	}
 
 	//reset button press counters:
-	left.downs = 0;
-	right.downs = 0;
-	up.downs = 0;
-	down.downs = 0;
+	button_down.downs = 0;
+	button_easy.downs = 0;
+	button_hard.downs = 0;
+
 }
 
 void PlayMode::draw(glm::uvec2 const &drawable_size) {
@@ -217,20 +340,21 @@ void PlayMode::draw(glm::uvec2 const &drawable_size) {
 		));
 
 		constexpr float H = 0.09f;
-		lines.draw_text("Mouse motion rotates camera; WASD moves; escape ungrabs mouse",
-			glm::vec3(-aspect + 0.1f * H, -1.0 + 0.1f * H, 0.0),
-			glm::vec3(H, 0.0f, 0.0f), glm::vec3(0.0f, H, 0.0f),
-			glm::u8vec4(0x00, 0x00, 0x00, 0x00));
 		float ofs = 2.0f / drawable_size.y;
-		lines.draw_text("Mouse motion rotates camera; WASD moves; escape ungrabs mouse",
+		if (gamestate == 1){
+			lines.draw_text("Click with z or x. Your score is " + std::to_string(int(score)),
 			glm::vec3(-aspect + 0.1f * H + ofs, -1.0 + + 0.1f * H + ofs, 0.0),
 			glm::vec3(H, 0.0f, 0.0f), glm::vec3(0.0f, H, 0.0f),
 			glm::u8vec4(0xff, 0xff, 0xff, 0x00));
+		} else {
+			lines.draw_text("Select difficulty, e for easy or h for hard.",
+			glm::vec3(-aspect + 0.1f * H + ofs, -1.0 + + 0.1f * H + ofs, 0.0),
+			glm::vec3(H, 0.0f, 0.0f), glm::vec3(0.0f, H, 0.0f),
+			glm::u8vec4(0xff, 0xff, 0xff, 0x00));
+		}
+
 	}
 	GL_ERRORS();
 }
 
-glm::vec3 PlayMode::get_leg_tip_position() {
-	//the vertex position here was read from the model in blender:
-	return lower_leg->make_local_to_world() * glm::vec4(-1.26137f, -11.861f, 0.0f, 1.0f);
-}
+
--- PlayMode.hpp
+++ PlayMode.hpp
@@ -23,21 +23,49 @@ struct PlayMode : Mode {
 	struct Button {
 		uint8_t downs = 0;
 		uint8_t pressed = 0;
-	} left, right, down, up;
+	} button_easy, button_hard, button_down;
+
+	float xangle;
+	float yangle;
+	const glm::quat default_rotation = glm::angleAxis(glm::radians(270.0f), glm::vec3(0.0f, 0.0f, 1.0f)) 
+	                              	 * glm::angleAxis(glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 
 	//local copy of the game scene (so code can change it during gameplay):
 	Scene scene;
+	std::vector<Scene::Transform*> cubea;
+	std::vector<Scene::Transform*> cubeb;
+	std::vector<Scene::Transform*> cubec;
+	std::vector<Scene::Transform*> cubed;
+	std::vector<Scene::Transform*> cubee;
+	std::vector<Scene::Transform*> cubef;
+	Scene::Transform* frame;
+	Scene::Transform* sphere;
 
-	//hexapod leg to wobble:
-	Scene::Transform *hip = nullptr;
-	Scene::Transform *upper_leg = nullptr;
-	Scene::Transform *lower_leg = nullptr;
-	glm::quat hip_base_rotation;
-	glm::quat upper_leg_base_rotation;
-	glm::quat lower_leg_base_rotation;
-	float wobble = 0.0f;
+	const float spawn_w = 7.0f;
+	const float spawn_h = 4.0f;
+	const float hit_distance = 25.0f;
 
-	glm::vec3 get_leg_tip_position();
+	const float time_int = 60.0f/80.0f/6.0f;
+	const float dist_int = 1.0f;
+	const float velocity = dist_int/time_int;
+
+	struct Block {
+		glm::vec3 pos; 
+		bool active = true; 
+		Scene::Transform* trans;
+	};
+	std::vector<Block> blocks;
+
+	struct Track {
+		std::vector<int> times;
+		std::vector<int> news;
+	} easy, hard; 
+	int difficulty = 0;
+
+	int gamestate = 0;
+
+	float score = 0.0f;
+	glm::vec3 player_pos;
 
 	//music coming from the tip of the leg (as a demonstration):
 	std::shared_ptr< Sound::PlayingSample > leg_tip_loop;
